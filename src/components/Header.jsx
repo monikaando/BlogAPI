4e456a53f0cdc542a86951e3a10e2227c607132e
Monika <mswidzinska87@gmail.com>
2020-04-19 13:16:36 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss'
+
+export default function Header() {
+    return ( 
+        <div className="header">
+            <img src="https://www.werkenbijsocialbrothers.nl/wp-content/uploads/2018/11/Foto-Social-Brothers-1.jpg" alt="social-brothers-header"/>
+            <div className="centered">
+                 <img src="https://socialbrothers.nl/wp-content/themes/social_brothers/assets/SBlogo.svg" alt="social-brothers-logo"/>
+            </div>
+        </div>
+      );
+}
+    
