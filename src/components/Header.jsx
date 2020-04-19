3cfb07c6790b80b863e793f7651a915315163fb9
Monika <mswidzinska87@gmail.com>
2020-04-19 13:20:57 +0200

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ export default function Header() {
             <img src="https://www.werkenbijsocialbrothers.nl/wp-content/uploads/2018/11/Foto-Social-Brothers-1.jpg" alt="social-brothers-header"/>
             <div className="centered">
                  <img src="https://socialbrothers.nl/wp-content/themes/social_brothers/assets/SBlogo.svg" alt="social-brothers-logo"/>
+                 <h1>test</h1>
             </div>
         </div>
       );
