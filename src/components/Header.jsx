7c2b6908c8879fbba1c196602b589d7dab7e773d
Monika <mswidzinska87@gmail.com>
2020-04-19 13:58:07 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,9 @@ import './Header.scss'
 export default function Header() {
     return ( 
         <div className="header">
-            <img src="https://www.werkenbijsocialbrothers.nl/wp-content/uploads/2018/11/Foto-Social-Brothers-1.jpg" alt="social-brothers-header"/>
+            <img className="header_img"src="https://www.werkenbijsocialbrothers.nl/wp-content/uploads/2018/11/Foto-Social-Brothers-1.jpg" alt="social-brothers-header"/>
             <div className="centered">
-                 <img src="https://socialbrothers.nl/wp-content/themes/social_brothers/assets/SBlogo.svg" alt="social-brothers-logo"/>
-                 <h1>test</h1>
+                 <img src="SocialBrothers-logo-white.png" alt="social-brothers-logo"/>
             </div>
         </div>
       );
